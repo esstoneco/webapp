ddf536c6639da15b40b6b2001076bd1dc9526e49
esstoneco <esstoneco@gmail.com>
2026-08-13 09:45:59 +0300

    Update import path for ProjectLightbox component

--- src/components/project/ProjectGallery.tsx
+++ src/components/project/ProjectGallery.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import ProjectLightbox from "webapp/src/components/project/ProjectLightbox.tsx";
+import ProjectLightbox from "./ProjectLightbox.tsx";
 
 type Props = {
   images: string[];
