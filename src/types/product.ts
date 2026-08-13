a803dedbe1f8b35281708beb65841e7f55f87b36
esstoneco <esstoneco@gmail.com>
2026-08-13 23:12:58 +0300

    Update product.ts

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,3 +1,12 @@
+export interface ProductSpecifications {
+  origin: string;
+  material: string;
+  finish: string;
+  density: string;
+  waterAbsorption: string;
+  compressiveStrength: string;
+}
+
 export interface Product {
   id: number;
   slug: string;
@@ -12,7 +21,9 @@ export interface Product {
   description: string;
 
   applications: string[];
-  specifications?: string[];
+
+  specifications: ProductSpecifications;
+
   color: string;
   finish: string;
   origin: string;
