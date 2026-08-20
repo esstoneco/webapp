ed45cee25dd789577934dccddd8fdaa38c796fc7
esstoneco <esstoneco@gmail.com>
2026-08-20 11:31:23 +0300

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -25,7 +25,7 @@ export const projects: Project[] = [
 
     marble: "Nestos Extra White",
 
-    image: "../assets/images/projects/bathrrom.jpg",
+    image: project1,
 
     gallery: [
       project1,
