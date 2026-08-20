9aec0c581e9d777f574ff9aaa0eb6f4a3b17bb4a
esstoneco <esstoneco@gmail.com>
2026-08-20 12:07:57 +0300

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ import type { Project } from "../types/project";
 // Temporary images.
 // Replace these imports with your actual project photographs later.
 
-import project1 from "../assets/images/projects/bathrrom.jpg";
+import project1 from "../assets/images/projects/eikona4.jpg";
 import project2 from "../assets/images/projects/lobbygerakina.jpg";
 import project3 from "../assets/images/projects/skales 2.jpg";
 import project4 from "../assets/images/projects/skales.jpg";
