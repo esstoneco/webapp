5fe72273d61509640c0c4e5e21ee1b69276d01e5
esstoneco <esstoneco@gmail.com>
2026-08-21 14:37:57 +0300

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -79,52 +79,8 @@ export default function Footer() {
       </div>
 
       <br />
-      <br />
-      <br /><br />
-      <br />
-		{ /* <div className="flex items-center justify-center min-h-screen">
-            <div className="col-md-4" >
-                <h4 data-i18n="ike_title">Στοιχεία Δημοσιότητας Ι.Κ.Ε.</h4>
-                <ul>
-                    <li><strong data-i18n="company_name_label">Επωνυμία:</strong> <span data-i18n="company_name">ES-STONE ΜΟΝΟΠΡΟΣΩΠΗ Ι.Κ.Ε.</span></li>
-                    <li><strong data-i18n="company_distinct_label">Διακριτικός Τίτλος:</strong> <span data-i18n="company_distinct">ES STONE Ι.Κ.Ε.</span></li>
-                    <li><strong data-i18n="company_address_label">Έδρα:</strong> <span data-i18n="company_address">[ΤΡΙΚΟΥΠΗ 5 / ΔΡΑΜΑ]</span></li>
-                    <li><strong data-i18n="company_vat_label">Α.Φ.Μ.:</strong> <span data-i18n="company_vat">803256732</span> (<span data-i18n="company_tax_office">ΔΟΥ</span>: <span data-i18n="tax_office_name">[Όνομα ΔΟΥ]</span>)</li>
-                    <li><strong data-i18n="company_gemi_label">Αριθμός Γ.Ε.ΜΗ.:</strong> <span data-i18n="company_gemi">193199319000</span></li>
-                    <li><strong data-i18n="company_chamber_label">Επιμελητήριο:</strong> <span data-i18n="company_chamber">[ΕΠΙΜΕΛΗΤΗΡΙΟ ΔΡΑΜΑΣ]</span></li>
-                </ul>
-            </div>
+      
 
-            <div className="col-md-4">
-                <h4 data-i18n="company_structure_title">Εταιρική Σύνθεση &amp; Κεφάλαιο</h4>
-                <ul >
-                    <li><strong data-i18n="company_capital_label">Συνολικό Κεφάλαιο:</strong> <span data-i18n="company_capital">[5000]</span> €</li>
-                    <li><strong data-i18n="company_manager_label">Διαχειριστής:</strong> <span data-i18n="company_manager">[Πύρρος Νικόπουλος]</span></li>
-                    <li><strong data-i18n="partners_label">Εταίροι &amp; Εισφορές:</strong></li>
-                   {/*<li>- <span data-i18n="partner1">[Όνομα Εταίρου 1]</span></li>
-                    <li>- <span data-i18n="partner2">[Όνομα Εταίρου 2]</span></li>*/}
-                    <li><strong data-i18n="company_kad_label">Κ.Α.Δ.:</strong> <span data-i18n="company_kad">[46830614,46830631]</span></li>
-                    {/*<li><a href="biochelanal.html"><h5 data-i18n="strengthanalysis">Physical Strength Analysis</h5></a></li>*/}
-					
-                </ul>
-            </div>
-             <div  className="col-md-4">
-                <h4 data-i18n="documents_title">Έγγραφα &amp; Πολιτικές</h4>
-                <ul>
-                    <li><strong data-i18n="financial_statements_label">Οικονομικές Καταστάσεις:</strong></li>
-                    <li><a href="downloads/isologismos-2025.pdf" target="_blank" data-i18n="balance_2025">Ισολογισμός 2026</a></li>
-                    <li ><strong data-i18n="legal_label">Νομικά:</strong></li>
-                    <li><a href="privacy-policy.html" data-i18n="privacy_policy">Πολιτική Απορρήτου</a></li>
-                    <li><a href="cookies-policy.html" data-i18n="cookies_policy">Cookies</a></li>
-                    <li><a href="terms.html" data-i18n="terms_use">Όροι Χρήσης</a></li>
-                    
-                    
-                </ul>
-            </div>
-
-        </div>
-        
-	  */}
 
       <div className="border-t border-stone-800 py-6 text-center text-stone-500">
 
