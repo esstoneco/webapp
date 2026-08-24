220bc902be604931be16f57f4d37963fd9807690
esstoneco <esstoneco@gmail.com>
2026-08-24 18:57:46 +0300

    Update project5 image import path

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,7 +7,7 @@ import project1 from "../assets/images/projects/eikona4.jpg";
 import project2 from "../assets/images/projects/lobbygerakina.jpg";
 import project3 from "../assets/images/projects/skales 2.jpg";
 import project4 from "../assets/images/projects/skales.jpg";
-import project5 from "../assets/images/projects/";
+import project5 from "../assets/images/projects/file_00000000114c81f59c0d193a76a2c8b5.png";
 
 
 export const projects: Project[] = [
