32c2e3b540f57f069e7d94e044b36795c15d1d6b
esstoneco <esstoneco@gmail.com>
2026-08-24 19:51:22 +0300

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ import type { Project } from "../types/project";
 
 import project1 from "../assets/images/projects/eikona4.jpg";
 import project2 from "../assets/images/projects/file_000000005ad08243a870a4b91fbcae45.png";
-import project3 from "../assets/images/projects/skales 2.jpg";
+import project3 from "../assets/images/projects/file_00000000ea1c81f4ba73bd682a7465e9.png";
 import project4 from "../assets/images/projects/file_00000000152881f489c15585598af21c.png";
 import project5 from "../assets/images/projects/file_00000000114c81f59c0d193a76a2c8b5.png";
 
