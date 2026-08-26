dfeb833ee5f391b93e5747382f1e9077cfe644b9
esstoneco <esstoneco@gmail.com>
2026-08-26 10:32:11 +0300

    Refactor Lawfiles component structure and content

--- src/components/home/Lawfiles.tsx
+++ src/components/home/Lawfiles.tsx
@@ -4,49 +4,8 @@ return (
 
 <section className="mx-auto max-w-7xl px-8 py-24">
 
-<div className="row">
-            <div className="col-md-4">
-                <h4 data-i18n="ike_title">Στοιχεία Δημοσιότητας Ι.Κ.Ε.</h4>
-                <ul>
-                    <li><strong data-i18n="company_name_label">Επωνυμία:</strong> <span data-i18n="company_name"> ES STONE IKE</span></li>
-                    <li><strong data-i18n="company_distinct_label">Διακριτικός Τίτλος:</strong> <span data-i18n="company_distinct">ES STONE ΜΟΝΟΠΡΟΣΩΠΗ IKE.</span></li>
-                    <li><strong data-i18n="company_address_label">Έδρα:</strong> <span data-i18n="company_address">[/ ΔΡΑΜΑ]</span></li>
-                    <li><strong data-i18n="company_vat_label">Α.Φ.Μ.:</strong> <span data-i18n="company_vat">999999999</span> (<span data-i18n="company_tax_office">ΔΟΥ</span>: <span data-i18n="tax_office_name">[Όνομα ΔΟΥ]</span>)</li>
-                    <li><strong data-i18n="company_gemi_label">Αριθμός Γ.Ε.ΜΗ.:</strong> <span data-i18n="company_gemi">123456789000</span></li>
-                    <li><strong data-i18n="company_chamber_label">Επιμελητήριο:</strong> <span data-i18n="company_chamber">[Όνομα Επιμελητηρίου]</span></li>
-                </ul>
-            </div>
 
-            <div className="col-md-4">
-                <h4 data-i18n="company_structure_title">Εταιρική Σύνθεση &amp; Κεφάλαιο</h4>
-                <ul>
-                    <li><strong data-i18n="company_capital_label">Συνολικό Κεφάλαιο:</strong> <span data-i18n="company_capital">[Ποσό]</span> €</li>
-                    <li><strong data-i18n="company_manager_label">Διαχειριστής:</strong> <span data-i18n="company_manager">[Ονοματεπώνυμο]</span></li>
-                    <li><strong data-i18n="partners_label">Εταίροι &amp; Εισφορές:</strong></li>
-                    <li>- <span data-i18n="partner1">[Όνομα Εταίρου 1]</span></li>
-                    <li>- <span data-i18n="partner2">[Όνομα Εταίρου 2]</span></li>
-                    <li><strong data-i18n="company_kad_label">Κ.Α.Δ.:</strong> <span data-i18n="company_kad">[Προσθέστε ΚΑΔ]</span></li>
-                </ul>
-            </div>
-             <div className="col-md-4">
-                <h4 data-i18n="documents_title">Έγγραφα &amp; Πολιτικές</h4>
-                <ul>
-                    <li><strong data-i18n="financial_statements_label">Οικονομικές Καταστάσεις:</strong></li>
-                    <li><a href="downloads/isologismos-2025.pdf" target="_blank" data-i18n="balance_2025">Ισολογισμός 2025</a></li>
-                    <li ><strong data-i18n="legal_label">Νομικά:</strong></li>
-                    <li><a href="privacy-policy.html" data-i18n="privacy_policy">Πολιτική Απορρήτου</a></li>
-                    <li><a href="cookies-policy.html" data-i18n="cookies_policy">Cookies</a></li>
-                    <li><a href="terms.html" data-i18n="terms_use">Όροι Χρήσης</a></li>
-                    <li><a href="biochelanal.html"><h5 data-i18n="strengthanalysis">Physical Strength Analysis</h5></a></li>
-					
-                    <div className="col-lg-3">
-				
-						
-						
-                
-            </div>
-        </ul></div>
-    </div>
+      
 
 </section>
 
